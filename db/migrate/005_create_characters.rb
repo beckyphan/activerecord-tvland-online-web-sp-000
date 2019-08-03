4516d86275d99e0ef57072082b4c7315ced03c57
Becky Phan <beckyphan@live.com>
2019-08-03 13:04:39 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,9 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    create_table :actors do |t|
+    create_table :characters do |t|
       #primary key of :id is created for us!
-      t.string :first_name
-      t.string :last_name
+      t.string :name
+      t.integer :actor_id 
+      t.integer :show_id
     end
   end
 end
