493a0eb7800a1f6f068bfdb14c22c44c388842b4
Becky Phan <beckyphan@live.com>
2019-08-03 13:47:21 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ActiveRecord::Base
   
-  has_many :characters
-  has_many :shows, through: :characters
+  has_many :character
+  has_many :show, through: :character
   
   def full_name
     full_name = "#{self.first_name} #{self.last_name}"
