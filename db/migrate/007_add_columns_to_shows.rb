b094b1cfcfa80884f8387f6d2e3a685156bd6474
Becky Phan <beckyphan@live.com>
2019-08-03 14:05:57 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -2,6 +2,6 @@ class AddColumnsToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
-    add_column :shows, :season, :integer
+    add_column :shows, :season, :string
   end
 end
